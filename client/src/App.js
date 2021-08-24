e9057bdc2eaf5d932ad219f9210ff8681153d752
LaurentBenazet <benazetlaurent@gmail.com>
2021-08-25 00:31:40 +0200

    Added prettier

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,17 @@ function App() {
 
   React.useEffect(() => {
     fetch("/api")
-        .then((res) => res.json())
-        .then((data) => setData(data.message));
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
   }, []);
 
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{!data ? "Loading..." : data}</p>
-        </header>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
   );
 }
 
